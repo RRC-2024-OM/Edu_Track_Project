a8fab086750c3314c4613d485ac317dc21297161
OM J PATEL <opatel@academic.rrc.ca>
2025-04-07 14:20:56 -0500

    Refactored app setup: streamline Swagger integration and update server port to 3000

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,23 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { authRoutes } from './routes/auth.routes';
-import swaggerUi from 'swagger-ui-express';
-import YAML from 'yamljs';
-import { errorHandler } from './middleware/error.middleware';
+import authRoutes from './routes/auth.routes';
+import { setupSwagger } from './config/swagger'; 
+
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
-// Swagger
-const swaggerDoc = YAML.load('./src/docs/swagger.yaml');
-app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+// Swagger setup
+setupSwagger(app);
 
 // Routes
 app.use('/api/auth', authRoutes);
 
-// Error Middleware
-app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
