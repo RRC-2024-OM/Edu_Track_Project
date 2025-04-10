5013524e3ef52e39e1764e8dd78bf1ede30b2bec
OM J PATEL <opatel@academic.rrc.ca>
2025-04-10 10:35:22 -0500

    Fix route imports in testApp to ensure user routes are correctly attached

--- test/utils/testApp.ts
+++ test/utils/testApp.ts
@@ -1,13 +1,12 @@
 import express from 'express';
 import authRoutes from '../../src/routes/auth.routes';
-// import userRoutes from '../../src/routes/user.routes';
+import userRoutes from '../../src/routes/user.routes'; 
 
 const app = express();
 
 app.use(express.json());
 
-// Attach your routes here
 app.use('/auth', authRoutes);
-// app.use('/users', userRoutes);
+app.use('/users', userRoutes); 
 
 export default app;
