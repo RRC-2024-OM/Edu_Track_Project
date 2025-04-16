974fd6f550ba76a7b819c1c86ad5c58b8af4424c
OM J PATEL <opatel@academic.rrc.ca>
2025-04-16 09:51:40 -0500

    Fix import statement for enrollmentRoutes and ensure proper route setup in app

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import authRoutes from './routes/auth.routes';
 import userRoutes from './routes/user.routes';
 import courseRoutes from './routes/course.routes';
+import enrollmentRoutes from './routes/enrollment.routes'
 import { setupSwagger } from './config/swagger'; 
 
 
@@ -21,8 +22,12 @@ setupSwagger(app);
 app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
 app.use('/courses', courseRoutes);
+app.use('/enrollments', enrollmentRoutes);
 
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
+
+export default app;
+
